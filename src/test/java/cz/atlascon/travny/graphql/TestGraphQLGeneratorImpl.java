396decda155890ec00907df2358150bdfa384f86
kribsky <kribsky@atlascon.cz>
2017-06-07 09:17:04 +0200

    wip

--- src/test/java/cz/atlascon/travny/graphql/TestGraphQLGeneratorImpl.java
+++ src/test/java/cz/atlascon/travny/graphql/TestGraphQLGeneratorImpl.java
@@ -1,15 +1,13 @@
 package cz.atlascon.travny.graphql;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import cz.atlascon.travny.schemas.RecordSchema;
-import graphql.language.Type;
+import cz.atlascon.travny.schemas.Schema;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLScalarType;
 import graphql.schema.GraphQLSchema;
 import org.junit.Assert;
 import org.junit.Test;
-
-import static cz.atlascon.travny.graphql.DummyClass.INT_NUMBER_NAME;
 
 /**
  * Created by tomas on 6.6.17.
@@ -17,21 +15,61 @@ import static cz.atlascon.travny.graphql.DummyClass.INT_NUMBER_NAME;
 public class TestGraphQLGeneratorImpl {
     private final GraphQLGenerator generator = new GraphQLGeneratorImpl();
 
+    private static final String INT_NUMBER_NAME = "intNumber";
+    private static final String STRING_NAME = "string";
+    private static final String LONG_NUMBER_NAME = "longNumber";
+    private static final String BYTES_NAME = "bytes";
+    private static final String BOOLEAN_NAME = "aboolean";
+    private static final String FLOAT_NAME = "floatNumber";
+
     @Test
     public void shouldProduceSomething() {
-        RecordSchema recordSchema = new DummyClass().getSchema();
+        RecordSchema recordSchema = createDummySchema();
         GraphQLSchema graphQLSchema = generator.generateSchema(recordSchema);
         Assert.assertNotNull(graphQLSchema);
     }
 
+    private RecordSchema createDummySchema(){
+        return RecordSchema.newBuilder(this.getClass().getName())
+                .addField(Schema.INT, INT_NUMBER_NAME)
+                .addField(Schema.STRING, STRING_NAME)
+                .addField(Schema.LONG, LONG_NUMBER_NAME)
+                .addField(Schema.BOOLEAN, BOOLEAN_NAME)
+                .addField(Schema.FLOAT, FLOAT_NAME)
+                //TODO proverit
+//                .addField(Schema.BYTES, BYTES_NAME)
+                .build();
+    }
+
     @Test
     public void shouldProduceValid() {
-        RecordSchema recordSchema = new DummyClass().getSchema();
+        RecordSchema recordSchema = createDummySchema();
         GraphQLSchema graphQLSchema = generator.generateSchema(recordSchema);
 
         GraphQLObjectType queryType = graphQLSchema.getQueryType();
-        GraphQLFieldDefinition fieldDefinition = queryType.getFieldDefinition(INT_NUMBER_NAME);
-        Type type = fieldDefinition.getDefinition().getType();
+        GraphQLFieldDefinition intDefinition = queryType.getFieldDefinition(INT_NUMBER_NAME);
+        Assert.assertTrue(intDefinition.getType() instanceof GraphQLScalarType);
+        Assert.assertEquals("Int", intDefinition.getType().getName());
+
+        GraphQLFieldDefinition stringDefinition = queryType.getFieldDefinition(STRING_NAME);
+        Assert.assertTrue(stringDefinition.getType() instanceof GraphQLScalarType);
+        Assert.assertEquals("String", stringDefinition.getType().getName());
+
+        GraphQLFieldDefinition longDefinition = queryType.getFieldDefinition(LONG_NUMBER_NAME);
+        Assert.assertTrue(longDefinition.getType() instanceof GraphQLScalarType);
+        Assert.assertEquals("Long", longDefinition.getType().getName());
+
+        GraphQLFieldDefinition booleanDefinition = queryType.getFieldDefinition(BOOLEAN_NAME);
+        Assert.assertTrue(booleanDefinition.getType() instanceof GraphQLScalarType);
+        Assert.assertEquals("Boolean", booleanDefinition.getType().getName());
+
+        GraphQLFieldDefinition floatDefinition = queryType.getFieldDefinition(FLOAT_NAME);
+        Assert.assertTrue(floatDefinition.getType() instanceof GraphQLScalarType);
+        Assert.assertEquals("Float", floatDefinition.getType().getName());
+
+        GraphQLFieldDefinition bytesDefinition = queryType.getFieldDefinition(BYTES_NAME);
+        Assert.assertTrue(bytesDefinition.getType() instanceof GraphQLScalarType);
+        Assert.assertEquals("Bytes", bytesDefinition.getType().getName());
     }
 
     @Test
@@ -41,6 +79,16 @@ public class TestGraphQLGeneratorImpl {
 
     @Test
     public void shouldProduceValidEmptyClassSchema() {
+
+    }
+
+    @Test
+    public void shouldProduceValidMultiClassSchema() {
+
+    }
+
+    @Test
+    public void shouldProduceValidExtendedSchema() {
 
     }
 
