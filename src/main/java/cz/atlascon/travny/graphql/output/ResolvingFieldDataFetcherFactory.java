4eecd3c63a253902104c70d66aa4034d44d8f628
CIGANKOVABNB1\Barbora Cigánková <cigankova@atlascon.cz>
2017-08-21 15:43:43 +0200

    fix list of enum

--- src/main/java/cz/atlascon/travny/graphql/output/ResolvingFieldDataFetcherFactory.java
+++ src/main/java/cz/atlascon/travny/graphql/output/ResolvingFieldDataFetcherFactory.java
@@ -68,7 +68,7 @@ public class ResolvingFieldDataFetcherFactory implements TravnyFieldDataFetcherF
 
                 if (val != null && val instanceof Enum) {
                     val = new EnumConstantImpl(((EnumConstant) val).getOrd(), ((EnumConstant) val).getConstant());
-                } else if (val instanceof List && ((List) val).get(0) instanceof Enum) {
+                } else if (val instanceof List && !((List) val).isEmpty() && ((List) val).get(0) instanceof Enum) {
                     List<EnumConstant> result = Lists.newArrayList();
                     ((List) val).forEach(v -> result.add(new EnumConstantImpl(((EnumConstant) v).getOrd(), ((EnumConstant) v).getConstant())));
                     val = result;
