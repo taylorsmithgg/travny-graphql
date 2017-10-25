6e7de40a6ce66291572e3df3379f9c243ef15ee0
kribsky <kribsky@atlascon.cz>
2017-10-25 08:29:18 +0200

    doc

--- src/main/java/cz/atlascon/travny/graphql/TravnyFetcher.java
+++ src/main/java/cz/atlascon/travny/graphql/TravnyFetcher.java
@@ -5,6 +5,10 @@ import graphql.schema.DataFetcher;
 import java.lang.reflect.ParameterizedType;
 import java.util.Collection;
 
+/**
+ * It needs to be abstract class beacse when it is interface, method 'getFetchedClass' doesnt return concrete type of <T>
+ * @param <T>
+ */
 public abstract class TravnyFetcher<T> implements DataFetcher<Collection<T>> {
 
     Class<T> getFetchedClass(){
