5f3cf8dad56c287802849d5c59cd851e7e5179da
kribsky <kribsky@atlascon.cz>
2017-10-25 08:35:02 +0200

    doc

--- src/main/java/cz/atlascon/travny/graphql/TravnyFetcher.java
+++ src/main/java/cz/atlascon/travny/graphql/TravnyFetcher.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 
 /**
  * It needs to be abstract class beacse when it is interface, method 'getFetchedClass' doesnt return concrete type of <T>
+ *     Collection is enforced because we add rootField always as list
  * @param <T>
  */
 public abstract class TravnyFetcher<T> implements DataFetcher<Collection<T>> {
